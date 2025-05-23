00c261a6ec5d24f020e1bae66cb5260454a83494
MuhammadaminPulatov <muhammadamin.pvlatov@gmail.com>
2025-05-23 15:56:31 +0500

    first commit

--- form/models.py
+++ form/models.py
@@ -1,11 +1,14 @@
-from django.db import models
-from django.db.models import Model, CharField, IntegerField
+from django.db.models import Model, CharField, IntegerField, TextField
 
 
 # Create your models here.
 class Product(Model):
     name=CharField(max_length=255)
+    desc=TextField()
     price=IntegerField()
+    count=IntegerField()
+    rating = IntegerField(default=0)
+
 
     def __str__(self):
         return self.name
